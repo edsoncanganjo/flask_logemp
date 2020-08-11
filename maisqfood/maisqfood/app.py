ea6d819e1c71c558e4a80b1367ee6170422561ae
Edson Canganjo <edsoncanganjo@hotmail.com>
2020-08-11 22:39:02 +0100

    Adding templates and debug toolbar

--- maisqfood/maisqfood/app.py
+++ maisqfood/maisqfood/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from maisqfood.ext import site
+from maisqfood.ext import config
+from maisqfood.ext import toolbar
 
 def create_app():
     app = Flask(__name__)
+    config.init_app(app)
+    toolbar.init_app(app)
     site.init_app(app)
     return app
