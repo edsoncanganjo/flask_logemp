0b7c98e29ee11f2eca54cbba4b512a6b36bc0265
Edson Canganjo <edsoncanganjo@hotmail.com>
2020-08-11 22:39:02 +0100

    Adding templates and debug toolbar

--- maisqfood/maisqfood/ext/site/main.py
+++ maisqfood/maisqfood/ext/site/main.py
@@ -1,11 +1,18 @@
-from flask import request, render_template
+from flask import render_template
 from flask import Blueprint
+from flask import current_app
 
 bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
-    return render_template(
-        "index.html",
-        name=request.args['name']
-        )
+    current_app.logger.debug("Cá estou ...")
+    return render_template("index.html")
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+@bp.route("/restauracao")
+def restaurants():
+    return render_template("restaurants.html")
