5ea93657445a3edbe11712a5712d7000e8c0b8ab
Edson Canganjo <edsoncanganjo@hotmail.com>
2020-08-13 21:00:28 +0100

    CLI update

--- maisqfood/maisqfood/ext/cli/__init__.py
+++ maisqfood/maisqfood/ext/cli/__init__.py
@@ -4,13 +4,12 @@ def init_app(app):
     @app.cli.command()
     def create_db():
         """ Este comando inicializa o db """
-        try:
-            db.create_all()
+        db.create_all()
 
-@app.cli.command()
+    @app.cli.command()
     def listar_pedidos():
         return "lista de pedidos"
 
-@app.cli.command()
+    @app.cli.command()
     def listar_usuarios():
         return "lista de usuarios"
