c2179cbce4db185cb878d0b50591181481b1a8f4
Edson Canganjo <edsoncanganjo@hotmail.com>
2020-08-16 03:55:27 +0100

    databases creaction

--- maisqfood/maisqfood/ext/cli/__init__.py
+++ maisqfood/maisqfood/ext/cli/__init__.py
@@ -1,4 +1,7 @@
+import click
 from maisqfood.ext.db import db
+from maisqfood.ext.site import models 
+
 
 def init_app(app):
     @app.cli.command()
@@ -7,9 +10,26 @@ def init_app(app):
         db.create_all()
 
     @app.cli.command()
+    @click.option("--email", "-e")
+    @click.option("--passwd", "-p")
+    @click.option("--admin", "-a", is_flag=True, default=False)
+    def add_user(email, passwd, admin):
+        """Adiciona novo usuário """
+        user = models.User(
+        email=email,
+        passwd=passwd,
+        admin=admin
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        click.echo(f"Usuário {email} criado com sucesso!")
+
+    @app.cli.command()
     def listar_pedidos():
-        return "lista de pedidos"
+        # TODO: usar tabulate
+        click.echo("lista de pedidos")
 
     @app.cli.command()
     def listar_usuarios():
-        return "lista de usuarios"
+        click.echo("lista de usuarios")
